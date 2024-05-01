db2a98cb2f5af2765d01aecd207f2a4e56f7642e
leonardorafaelli <leonardorafaelli@gmail.com>
2024-05-01 17:07:19 +0200

    feat: adds user infos in home_page, after one is logged in

--- src/main/java/com/javastudies/login_system/controller/UserController.java
+++ src/main/java/com/javastudies/login_system/controller/UserController.java
@@ -1,12 +1,30 @@
 package com.javastudies.login_system.controller;
 
+import com.javastudies.login_system.model.UserDtls;
+import com.javastudies.login_system.repository.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.security.Principal;
 
 @Controller
 @RequestMapping("/user/")
 public class UserController {
+
+    @Autowired
+    UserRepository userRepo;
+
+    @ModelAttribute
+    private void userDetails(Model m, Principal p) {
+        String userEmail = p.getName();
+        UserDtls user = userRepo.findByEmail(userEmail);
+
+        m.addAttribute("user", user);
+    }
 
     @GetMapping("")
     public String home(){
